efefd7b2230988564deaf0a300737466465c93f8
hosose <hosose22@gmail.com>
2022-11-30 21:47:08 +0900

    Add: Social Login

---
+++ src/models/userDao.js
@@ -0,0 +1,64 @@
+const { database } = require("./dataSource");
+
+const createUser = async (kakaoId, name, email, socialType = 1) => {
+  const result = await database.query(
+    `INSERT INTO 
+      users (
+        email,
+        social_id,
+        social_type_id,
+        nickname
+        )
+      VALUES
+        (?, ?, ?, ?);`,
+    [email, kakaoId, socialType, name]
+  );
+
+  return result;
+};
+
+const getUserByKaKaoId = async (kakaoId) => {
+  const result = await database.query(
+    `SELECT
+      id
+    FROM
+      users
+    WHERE
+      social_id=?;`,
+    [kakaoId]
+  );
+
+  return result[0];
+};
+
+const updateUserInfo = async (kakaoId, name, email) => {
+  return await database.query(
+    `
+        UPDATE users
+        SET
+            nickname=?,
+            email=?
+        WHERE social_id=?;`,
+    [name, email, kakaoId]
+  );
+};
+
+const getUserById = async (id) => {
+  const result = await database.query(
+    `
+		SELECT 
+			id
+		FROM users
+		WHERE id=?`,
+    [id]
+  );
+
+  return result[0];
+};
+
+module.exports = {
+  createUser,
+  getUserByKaKaoId,
+  updateUserInfo,
+  getUserById,
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,9 @@ const express = require("express");
 const router = express.Router();
 
 const projectRouter = require("./projectRouter");
+const userRouter = require("./userRouter");
 
 router.use("/projects", projectRouter);
+router.use("/user", userRouter);
 
 module.exports = router;
